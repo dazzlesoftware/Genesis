ef6c5e5d5ce3a7718db00053c8dfa2a45d0c4675
Dazzle Software, LLC <dazzle.software@gmail.com>
2025-01-29 17:22:22 -0700

    Update StylesEvent.php

--- src/classes/Gantry/Admin/Events/StylesEvent.php
+++ src/classes/Gantry/Admin/Events/StylesEvent.php
@@ -14,13 +14,13 @@ namespace Gantry\Admin\Events;
 use Gantry\Component\Controller\RestfulControllerInterface;
 use Gantry\Framework\Gantry;
 use Gantry\Framework\Theme;
-use DazzleSoftware\Toolbox\Event\Event;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
  * Class StylesEvent
  * @package Gantry\Admin\Events
  */
-class StylesEvent extends Event
+class StylesEvent extends EventDispatcher
 {
     /** @var Gantry */
     public $gantry;
